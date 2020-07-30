fc14bd58b052b68bc1939b3a4e44fe022971602a
Harsh Agarwal <harsh29092000@gmail.com>
2020-07-30 18:11:49 +0530

    Added templates for Home and About page

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts= [
+    {
+        'author': 'Harsh Agarwal',
+        'topic': 'Data Science',
+        'date_posted': '30 July 2020',
+        'content': 'Content no 1'
+    },
+    {
+        'author': 'John Doe',
+        'topic': 'Data Science',
+        'date_posted': '31 July 2020',
+        'content': 'Content no 2'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context={
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
