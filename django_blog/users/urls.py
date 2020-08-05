0e2de20cad539ce09193018b60b4e3a063867d97
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-05 15:32:54 +0530

    Added a login and logout system

--- django_blog/users/urls.py
+++ django_blog/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('register/', views.register),
-    # path('about/', views.about, name='blog-about'),
+    path('register/', views.register, name= 'user-register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name= 'user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name= 'user-logout')
 ]
