6311b33bbdd715dd0cb1bc49f18c19eb08ebbc26
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-24 14:06:39 +0530

    Added Class based views and Detailed Post view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -10,6 +10,15 @@ def home(request):
         'title': 'Home'
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
