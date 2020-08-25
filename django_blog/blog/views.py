f22ddf4baeea60c92087df46c7415427bdbc1da5
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-25 11:35:50 +0530

    Added Pagination to the main list view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -14,6 +14,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = Post
