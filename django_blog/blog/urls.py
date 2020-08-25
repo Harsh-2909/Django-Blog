387909fc517766e5069fcae21b00868339d69840
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-25 12:00:31 +0530

    Added User blog post pagination

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,12 +3,14 @@ from .views import (PostListView,
 PostDetailView,
 PostCreateView,
 PostUpdateView,
-PostDeleteView
+PostDeleteView,
+UserPostListView
 )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'), # <app>/<model>_<viewtype>.html
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'), # <app>/<model>_<viewtype>.html
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'), # <app>/<model>_<viewtype>.html
     path('post/new/', PostCreateView.as_view(), name='post-create'), # <app>/<model>_<viewtype>.html
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), # <app>/<model>_<viewtype>.html
