a28f0a8180201ff8cf74e8ace71f1f9b5ee5fe70
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-26 13:41:17 +0530

    Added Mailing System for Password Reset

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,4 +136,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get()
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
--- django_blog/users/urls.py
+++ django_blog/users/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
     name= 'password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',
     auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
-    name= 'password_reset_confirm')
+    name= 'password_reset_confirm'),
+    path('password-reset-complete/',
+    auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+    name= 'password_reset_complete'),
 ]
