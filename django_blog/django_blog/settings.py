2da062ae8212987381905ff1ecc85fef39ba8275
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-27 14:01:42 +0530

    Static files path added for deployment

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
