fefdc5eecf67cd596cca03769e539f74f8e066d7
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-27 16:45:43 +0530

    Added environment variable for secret key

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q#=#28)c5wz@ry2y0^z455m%($tv*gt_unbn%rudgnm%*i&v9s'
+# SECRET_KEY = 'q#=#28)c5wz@ry2y0^z455m%($tv*gt_unbn%rudgnm%*i&v9s'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
