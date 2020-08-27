5f7242cc269e735c35938250362fee60c4644e1a
Harsh Agarwal <harsh29092000@gmail.com>
2020-08-27 14:13:41 +0530

    Added Procfile and set DEBUG = False

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q#=#28)c5wz@ry2y0^z455m%($tv*gt_unbn%rudgnm%*i&v9s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
